bcc75fa4e35b482271bc9452b4b555ffc4c9803d
harsh-dynasty <harshsoni0310@gmail.com>
2021-02-02 12:41:21 +0530

    final

--- server.js
+++ server.js
@@ -47,3 +47,48 @@ app.post('/addNotification',(req,res)=>{
     notifications.collection.insertOne(doc).then(data=>res.json({status:'success'}))
 
 })
+
+const client= new google.auth.JWT(
+    keys.client_email,
+    null,
+    keys.private_key,
+    ["https://www.googleapis.com/auth/spreadsheets"]
+);
+
+client.authorize((err,tokens)=>{
+    if(err){
+        console.log(err);
+        return;
+    }else{
+        console.log("Spreadsheet Connected");
+        
+    }
+
+})
+
+function gsrun(cl,newRow){
+    const gsapi=google.sheets({version:'v4',auth:cl});
+
+    const options={
+        spreadsheetId:"1-NL_D1QgV3_4f4tfl2k4y_MlKu-m7kvN49DXSgZJvDQ",
+        range:"Sheet1!A1",
+        
+        valueInputOption:'USER_ENTERED',
+        
+        resource:{values:[newRow]}
+    };
+    gsapi.spreadsheets.values.append(options);
+   
+        
+}
+
+app.post('/contactus',(req,res)=>{
+    
+    var email=req.body.email;
+    var subject=req.body.subject;
+    var message=req.body.message;
+    
+    var newRow=[email,subject,message];
+    gsrun(client,newRow);
+    res.json({mssg:"Success"});
+})
